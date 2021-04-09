5ace31ab605191fa6742d473d40bfd9a489f26e3
Alice Menzie <alice.gmail.com>
2021-04-09 10:46:40 +1000

    added command line arguments

--- run.rb
+++ run.rb
@@ -1,4 +1,15 @@
 require_relative 'index.rb'
 
-app = App.new
-app.run
+command_arg = ARGV 
+    if command_arg[0] == '--help'
+        puts "--User Guide --"
+        puts "How do I run this application? "
+        puts " aoe_pt.sh"
+        puts "What Command Line Arguments are Available? "
+        puts "--help,  "
+        puts "How do I use this Application? "
+    else 
+        app = App.new
+        app.run
+
+    end
