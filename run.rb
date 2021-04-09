696edf05e5370dd48cfc883343878021aa0b98b7
Alice Menzie <alice.gmail.com>
2021-04-09 11:16:54 +1000

    updating ARGV syntax and user guide

--- run.rb
+++ run.rb
@@ -1,15 +1,23 @@
 require_relative 'index.rb'
 
-command_arg = ARGV 
-    if command_arg[0] == '--help'
-        puts "--User Guide --"
-        puts "How do I run this application? "
-        puts " aoe_pt.sh"
-        puts "What Command Line Arguments are Available? "
-        puts "--help,  "
-        puts "How do I use this Application? "
-    else 
-        app = App.new
-        app.run
+first_arg, *command_arg = ARGV 
+ARGV.clear
+case first_arg
+when '--help'
+    puts "--User Guide --"
+    puts "How do I run this application? "
+    puts " aoe_pt.sh"
+    puts "What Command Line Arguments are Available? "
+    puts "--help  "
+    puts "How do I use this Application? "
+    puts "Once running the application, features will be displayed on the menu, to access each element use the arrow keys to move to the feature you want to access 1. Match History 2. Randmon Civ generation 3. Civ information 4. Exit, then press enter. Once a category has been selected continue moving through the app with a arrow keys and pressing enter when you wish to move to a different space in the application"
+      
+    
+# when '--start', '-s'
+#     app = App.new
+#     app.run
+else 
+    app = App.new
+    app.run
 
-    end
+end
