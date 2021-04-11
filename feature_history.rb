1929f126d32e369cf7546eb5c39005170107959d
Alice Menzie <alice.gmail.com>
2021-04-11 18:29:35 +1000

    refactoring feature history

--- feature_history.rb
+++ feature_history.rb
@@ -11,69 +11,66 @@ class History
     @civ = civ
     @result = result
     @civ_list = ["Aztecs",
-        "Berbers",
-        "Britons",
-        "Bulgarians",
-        "Burmese",
-        "Byzantines",
-        "Celts",
-        "Chinese",
-        "Cumans",
-        "Ethiopians",
-        "Franks",
-        "Goths",
-        "Huns",
-        "Incas",
-        "Indians",
-        "Italians",
-        "Japanese",
-        "Khmer",
-        "Koreans",
-        "Lithuanians",
-        "Magyars",
-        "Malay",
-        "Malians",
-        "Mayans",
-        "Mongols",
-        "Persians",
-        "Portuguese",
-        "Saracens",
-        "Slavs",
-        "Spanish",
-        "Tatars",
-        "Teutons",
-        "Turks",
-        "Vietnamese",
-        "Vikings "]
+                 "Berbers",
+                 "Britons",
+                 "Bulgarians",
+                 "Burmese",
+                 "Byzantines",
+                 "Celts",
+                 "Chinese",
+                 "Cumans",
+                 "Ethiopians",
+                 "Franks",
+                 "Goths",
+                 "Huns",
+                 "Incas",
+                 "Indians",
+                 "Italians",
+                 "Japanese",
+                 "Khmer",
+                 "Koreans",
+                 "Lithuanians",
+                 "Magyars",
+                 "Malay",
+                 "Malians",
+                 "Mayans",
+                 "Mongols",
+                 "Persians",
+                 "Portuguese",
+                 "Saracens",
+                 "Slavs",
+                 "Spanish",
+                 "Tatars",
+                 "Teutons",
+                 "Turks",
+                 "Vietnamese",
+                 "Vikings "]
   end
-
-  # def run_history
-  # end
 
   def get_civ
     loop do
-    puts "What Civ did you play?"
-    @civilisation = gets.chomp.capitalize
-    unless @civ_list.include?(@civilisation)
+      puts "What Civ did you play?"
+      @civilisation = gets.chomp.capitalize
+      unless @civ_list.include?(@civilisation)
         puts "please enter a valid civilisation"
-    else
+      else
         break
-    end
+      end
     end
     return @civilisation
   end
 
   def get_result
     loop do
-        puts "Did you win or loose?"
-        @result = gets.chomp
-        unless @result == "win" || @result == "lost"
-          puts "Accepted values are: win or lost, please try again"
-        else
-          break
-        end
+      puts "Did you win or loose?"
+      @result = gets.chomp
+      unless @result == "win" || @result == "lost"
+        puts "Accepted values are: win or lost, please try again"
+      else
+        break
       end
-      return @result
+    end
+    return @result
   end
 
   def add_history
@@ -84,18 +81,12 @@ class History
   def display_input
     add_history
     puts @history
-    # File.open("history.json", "w+")
-    # File.write("history.json", @history.to_json)
+
     puts "History Successfully updated"
   end
 end
 
-##############--------------END OF CLASS -----------------------
-
-# puts "#{new_history}"
-# new_history.add_history
-
-# file.write("history.json")@history.to_json
+# end of class
 
 def display_history_home
   system("clear")
@@ -116,23 +107,15 @@ end
 def process_landing(user_choice, history)
   case user_choice
   when 1
-    #Update History
     update_history(history)
-    #  write_json
-    #  back_to_main
-
   when 2
-    #View History
-    #  back_to_main
-    #  puts @history
-    file = File.read("history.json") ### this is the raw string
-    my_hash = JSON.parse(file) ##this is parse
+    file = File.read("history.json")
+    my_hash = JSON.parse(file)
     puts my_hash
   end
 end
 
 def update_history(display)
-  # puts "Civ: #{@civ} result: #{@result}"
   display.display_input
 end
 
